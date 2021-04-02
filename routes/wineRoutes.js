cc5d955cc6f7946d3509256608fd14d6fac49d39
Tim Mok <timothyjmok@gmail.com>
2021-04-02 19:58:31 -0400

    built wine routes

--- routes/wineRoutes.js
+++ routes/wineRoutes.js
@@ -2,9 +2,10 @@ const wineControllers = require('../controllers/wineControllers')
 const express = require('express')
 const wineRoutes = express.Router()
 
-
-
-
-
+wineRoutes.get('/', wineControllers.getAll)
+wineRoutes.get('/:id', wineControllers.getOne)
+wineRoutes.post('/', wineControllers.postNew)
+wineRoutes.put('/:id', wineControllers.putOld)
+wineRoutes.delete('/:id', wineControllers.deleteWine)
 
 module.exports = wineRoutes
