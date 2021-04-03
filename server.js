353ad9da0e50201fb7298da989d8124716c89c6c
Mason <masonbush751@gmail.com>
2021-04-03 12:03:04 -0500

    Stretch done

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
+const wineRoutes = require('./routes/wineRoutes')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 app.listen(PORT, () => {
@@ -11,5 +14,4 @@ app.listen(PORT, () => {
     routesReport.print()
 })
 
-const wineRoutes = require('./routes/wineRoutes')
 app.use('/wines', wineRoutes)
