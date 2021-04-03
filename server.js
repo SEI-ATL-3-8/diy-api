5a0cf50cc0fdf62e131236f9cd1799311a2ad882
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-04-02 22:08:29 -0400

    databaseSet, expressCreated

---
+++ server.js
@@ -0,0 +1,26 @@
+// 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const rowdy = require('rowdy-logger')
+
+const routesReport = rowdy.begin(app)
+
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+console.log(`Listening on port ${PORT}`)
+routesReport.print()
+})
+
