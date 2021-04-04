4cccd9d563f28d5527c1269c767f3044b8e6f7d6
noahlinn <n.linn95@gmail.com>
2021-04-04 18:56:52 -0400

    controllers & routes work, cors kinda works.

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const rowdy = require('rowdy-logger')
-const models = require('./models')
 const routesReport = rowdy.begin(app)
+const bookRoute = require('./routes/bookRoutes')
+
+const cors = require('cors')
+
+app.use('/books', cors(), bookRoute)
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
     routesReport.print()
 })
-const bookRoute = require('./routes/bookRoutes')
-app.use('/books', bookRoute)
-
-
-
-
