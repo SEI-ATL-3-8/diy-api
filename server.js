9b45d71c9f4e38076a9a8f254c3e03643158778c
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-04 15:37:26 -0500

    sunday commit

--- server.js
+++ server.js
@@ -4,85 +4,92 @@
 const rowdy = require('rowdy-logger')
 
 const routesReport = rowdy.begin(app)
-
 app.use(express.json())
-
 app.listen(PORT, () => {
-console.log(`Listening on port ${PORT}`)
-routesReport.print()
+    console.log(`listening on port ${PORT}`)
+    routesReport.print()
 })
 
- const models = require('./models')
+const wineRoutes = require('./routes/wineRoutes')
+app.use('/wines', wineRoutes)
+// app.use(express.json())
 
- const getAllWines = async (req, res) => {
-     try {
-         let wines = await models.wine.findAll()
+// app.listen(PORT, () => {
+// console.log(`Listening on port ${PORT}`)
+// routesReport.print()
+// })
 
-         res.json({wines})
-     } catch (error) {
-         res.json({error})
-     }
- }
+//  const models = require('./models')
 
- const getAWine = async (req, res) => {
-     try {
-         let wines = await models.wine.findOne({
-             where: {
-                 id: req.params.id
-             }
-         })
-         res.json({wine})
-     } catch (error) {
-         res.json({error})
-     }
- }
+//  const getAllWines = async (req, res) => {
+//      try {
+//          let wines = await models.wine.findAll()
 
- const updateWine = async (req, res) => {
-     try {
-         let updates = req.body
-         let wineToUpdate = await models.wine.findOne({
-             where: {
-                 id: req.params.id
-             }
-         })
-         let final = await wineToUpdate.update(updates)
-         res.json({final})
-     } catch (error) {
-         res.json({error})
-     }
- }
+//          res.json({wines})
+//      } catch (error) {
+//          res.json({error})
+//      }
+//  }
 
- const createWine = async (req, res) => {
-     try {
-         let newWine = await models.wine.create({
-             name: req.body.name,
-             price: req.body.price,
-             year: req.body.year,
-             grape: req.body.year,
-             country: req.body.country,
-             region: req.body.region
-         })
-         res.json({newWine})
-     } catch (error) {
-         res.json({error})
-     }
- }
+//  const getAWine = async (req, res) => {
+//      try {
+//          let wines = await models.wine.findOne({
+//              where: {
+//                  id: req.params.id
+//              }
+//          })
+//          res.json({wine})
+//      } catch (error) {
+//          res.json({error})
+//      }
+//  }
 
- const deleteWine = async(req, res) => {
-     try {
-         let noMoreWine = await models.wine.destroy({
-             where: {
-                 id: req.params.id
-             }
-         })
-         res.json({noMoreWine})
-     } catch (error) {
-         res.json({error})
-     }
- }
+//  const updateWine = async (req, res) => {
+//      try {
+//          let updates = req.body
+//          let wineToUpdate = await models.wine.findOne({
+//              where: {
+//                  id: req.params.id
+//              }
+//          })
+//          let final = await wineToUpdate.update(updates)
+//          res.json({final})
+//      } catch (error) {
+//          res.json({error})
+//      }
+//  }
 
- app.get('/wines', getAllWines)
- app.get('/wines/:id', getAWine)
- app.put('/wines/:id', updateWine)
- app.post('/wine', createWine)
- app.delete('/wines/:id', deleteWine)
+//  const createWine = async (req, res) => {
+//      try {
+//          let newWine = await models.wine.create({
+//              name: req.body.name,
+//              price: req.body.price,
+//              year: req.body.year,
+//              grape: req.body.grape,
+//              country: req.body.country,
+//              region: req.body.region
+//          })
+//          res.json({newWine})
+//      } catch (error) {
+//          res.json({error})
+//      }
+//  }
+
+//  const deleteWine = async(req, res) => {
+//      try {
+//          let noMoreWine = await models.wine.destroy({
+//              where: {
+//                  id: req.params.id
+//              }
+//          })
+//          res.json({noMoreWine})
+//      } catch (error) {
+//          res.json({error})
+//      }
+//  }
+
+//  app.get('/wines', getAllWines)
+//  app.get('/wines/:id', getAWine)
+//  app.put('/wines/:id', updateWine)
+//  app.post('/wine', createWine)
+//  app.delete('/wines/:id', deleteWine)
