d44bf67c301b22a9d0ebc95fda7fc3f958582578
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-04 18:55:42 -0400

    general steps done

---
+++ controllers/bookController.js
@@ -0,0 +1,15 @@
+// const models = require('../models')
+
+// const bookController = {}
+
+// bookController.getAll = async (req,res) => {
+//     try {
+//     let books = await models.book.findAll()
+
+//     res.json({books})
+// } catch (error) {
+//     res.json({error})
+//     }
+// }
+
+// module.exports = bookController;
---
+++ routes/bookRoutes.js
@@ -0,0 +1,11 @@
+// const bookController = require('../controllers/bookController')
+// const express = require('express')
+// const bookRoutes = express.Router()
+
+// bookRoutes.get('/', bookController.getAll)
+// bookRoutes.get('/:id', bookController.find)
+// bookRoutes.post('/', bookController.create)
+// bookRoutes.put('/:id', bookController.update)
+// bookRoutes.delete('/:id', bookController.delete)
+
+// module.exports = bookRoutes;
