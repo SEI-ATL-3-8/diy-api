3235d9668d4d6a3bbd6b47a3c05c160f81d898e1
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-04-04 11:40:20 -0400

    working

--- server.js
+++ server.js
@@ -10,90 +10,85 @@ const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
     // this follow everytime a new routes gets installed in the app shows its in iterm
 
-// const models = require('./models')
-const movieRoutes = require('./routes/moviesRoutes')
+const models = require('./models')
     // you need to define models in order to get access to it
-app.use('/movies', movieRoutes)
-    // const getAllMovies = async(req, res) => {
-    //     //named the function and tell it to get the info
-    //     // you need to define it to req and res or it wont know what to do
-    //     try {
-    //         let allMovies = await models.movie.findAll()
-    //             // find all the movies inside model        
-    //         res.json(allMovies)
-    //             // respond with the info in a json object         
-    //     } catch (error) {
-    //         res.json({ error })
-    //     }
-    // }
 
-// const getOneMovie = async(req, res) => {
-//     try {
-//         let oneMovie = await models.movie.findOne({
-//             where: {
-//                 id: req.params.id
-//                     // req is the request and the paramator is the / after the url and that is what the id should equal too.  
-//             }
-//         })
-//         res.json(oneMovie)
+const getAllMovies = async(req, res) => {
+    //named the function and tell it to get the info
+    // you need to define it to req and res or it wont know what to do
+    try {
+        let allMovies = await models.movie.findAll()
+            // find all the movies inside model        
+        res.json(allMovies)
+            // respond with the info in a json object         
+    } catch (error) {
+        res.json({ error })
+    }
+}
 
-//     } catch (error) {
-//         res.json({ error })
-//     }
-// }
+const getOneMovie = async(req, res) => {
+    try {
+        let oneMovie = await models.movie.findOne({
+            where: {
+                id: req.params.id
+                    // req is the request and the paramator is the / after the url and that is what the id should equal too.  
+            }
+        })
+        res.json(oneMovie)
 
-// const createMovie = async(req, res) => {
-//     try {
-//         let makeMovie = await models.movie.create({
-//             title: req.body.title,
-//             director: req.body.director
-//         })
-//         res.json({ makeMovie })
-//     } catch (error) {
-//         res.json(error)
-//     }
-// }
+    } catch (error) {
+        res.json({ error })
+    }
+}
 
-// const updateMovie = async(req, res) => {
-//     try {
-//         let update = req.body
-//         let newMovie = await models.movie.findOne({
-//                 where: {
-//                     id: req.params.id
-//                 }
-//             }) // go into movie and find the id with the same as the param
-//         let final = await newMovie.update(update)
-//             // when you find it wait for it and then update only the body
-//         res.json({ final })
-//             //once finished res in json the update
-//     } catch (error) {
-//         res.json({ error })
-//     }
-// }
+const createMovie = async(req, res) => {
+    try {
+        let makeMovie = await models.movie.create({
+            title: req.body.title,
+            director: req.body.director
+        })
+        res.json({ makeMovie })
+    } catch (error) {
+        res.json(error)
+    }
+}
 
-// const deleteMovie = async(req, res) => {
-//     try {
-//         let erase = models.movie.destroy({
-//             where: {
-//                 id: req.params.id
-//             }
-//         })
-//         res.json({ erase })
-//     } catch (error) {
-//         res.json({ error })
-//     }
-// }
+const updateMovie = async(req, res) => {
+    try {
+        let update = req.body
+        let newMovie = await models.movie.findOne({
+                where: {
+                    id: req.params.id
+                }
+            }) // go into movie and find the id with the same as the param
+        let final = await newMovie.update(update)
+            // when you find it wait for it and then update only the body
+        res.json({ final })
+            //once finished res in json the update
+    } catch (error) {
+        res.json({ error })
+    }
+}
 
-
-
-// app.get('/movies/:id', getOneMovie)
-// app.get('/movies', getAllMovies)
-// app.post('/movies', createMovie)
-// app.put('/movies/:id', updateMovie)
-// app.delete('/movies/:id', deleteMovie)
+const deleteMovie = async(req, res) => {
+    try {
+        let erase = models.movie.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({ erase })
+    } catch (error) {
+        res.json({ error })
+    }
+}
 
 
 
+app.get('/movies/:id', getOneMovie)
+app.get('/movies', getAllMovies)
+app.post('/movies', createMovie)
+app.put('/movies/:id', updateMovie)
 
 
 
