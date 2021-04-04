37c94d759b33b25ca9f3e6c42899b1f9d40ee54b
noahlinn <n.linn95@gmail.com>
2021-04-03 20:25:22 -0400

    it cruds

--- server.js
+++ server.js
@@ -32,7 +32,49 @@ const createBook = async (req, res) => {
         
     }
 }
+const oneBook = async (req, res) => {
+    try {
+        let oneBook = await models.book.findOne({
+            where:{
+               id: req.params.id 
+            }
+        })
+        res.json(oneBook)
+    } catch (error) {
+        res.json ({error})
+    }
+}
 
+const updateBook = async (req, res) => {
+    try {
+        let updates = req.body
+        let book = await models.book.findOne({
+            where:{
+                id: req.params.id
+            }
+        })
+        let updated = await book.update(updates)
+        res.json({updated})
+    } catch (error) {
+        res.json ({error})
+    }
+}
+
+const deleteBook = async (req, res) => {
+    try {
+        let deadBook = await models.book.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.json({deadBook})
+    } catch (error) {
+        res.json ({error})
+    }
+}
 
 app.get('/books', getAllBooks)
 app.post('/books', createBook)
+app.get('/books/:id', oneBook)
+app.put('/books/:id', updateBook)
+app.delete('/books/:id', deleteBook)
