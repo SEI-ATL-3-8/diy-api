39b4ff8335738af589aba74b5da1d2bce710c0ff
noahlinn <n.linn95@gmail.com>
2021-04-03 20:58:26 -0400

    created controller & route

--- server.js
+++ server.js
@@ -4,77 +4,13 @@ const PORT = process.env.PORT || 3000
 const rowdy = require('rowdy-logger')
 const models = require('./models')
 const routesReport = rowdy.begin(app)
-app.use(express.json())
-
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
     routesReport.print()
 })
+const bookRoute = require('./routes/bookRoutes')
+app.use('/books', bookRoute)
 
-const getAllBooks = async (req, res) => {
-    try {
-        let book = await models.book.findAll()
-        res.json({book})
-    } catch (error) {
-        res.json({error})
-    }
-}
-const createBook = async (req, res) => {
-    try {
-        let newBook = await models.book.create({
-            title: req.body.title,
-            author: req.body.author,
-            release_date: req.body.release_date,
-            img_url: req.body.img_url  
-        })
-        res.json({newBook})
-    } catch (error) {
-        
-    }
-}
-const oneBook = async (req, res) => {
-    try {
-        let oneBook = await models.book.findOne({
-            where:{
-               id: req.params.id 
-            }
-        })
-        res.json(oneBook)
-    } catch (error) {
-        res.json ({error})
-    }
-}
 
-const updateBook = async (req, res) => {
-    try {
-        let updates = req.body
-        let book = await models.book.findOne({
-            where:{
-                id: req.params.id
-            }
-        })
-        let updated = await book.update(updates)
-        res.json({updated})
-    } catch (error) {
-        res.json ({error})
-    }
-}
 
-const deleteBook = async (req, res) => {
-    try {
-        let deadBook = await models.book.destroy({
-            where:{
-                id: req.params.id
-            }
-        })
-        res.json({deadBook})
-    } catch (error) {
-        res.json ({error})
-    }
-}
 
-app.get('/books', getAllBooks)
-app.post('/books', createBook)
-app.get('/books/:id', oneBook)
-app.put('/books/:id', updateBook)
-app.delete('/books/:id', deleteBook)
