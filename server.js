810dd7cfaa3155b744622b1ca6571f45221bb8c7
noahlinn <n.linn95@gmail.com>
2021-04-05 10:19:48 -0400

    bug fix

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000
 const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
 const bookRoute = require('./routes/bookRoutes')
-
+app.use(express.json())
 const cors = require('cors')
-
+// app.use(express.json)
 app.use('/books', cors(), bookRoute)
 
 
